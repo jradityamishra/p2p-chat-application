ba25e5675c07eb1d5e778a11c29f365ee57eee4c
jradityamishra <106857292+jradityamishra@users.noreply.github.com>
2023-04-10 19:02:01 +0530

    back-end added

---
+++ Back-end/index.js
@@ -0,0 +1,48 @@
+const http=require("http");
+const express =require("express");
+const cors = require("cors");
+const socketIO = require("socket.io");
+
+const app=express();
+const port= 4500||process.env.PORT ;
+
+
+const users=[{}];
+
+app.use(cors());//cors used for intercommunication between URL
+app.get("/",(req,res)=>{
+    res.send("HELL ITS WORKING");
+})
+
+const server=http.createServer(app);
+
+const io=socketIO(server);
+
+server.listen(port,()=>{
+    console.log(`server is working at ${port}`)
+})
+io.on("connection",()=>{
+    console.log("new connection");
+})
+ io.on("connection",(socket)=>{
+     console.log("New Connection");
+
+     socket.on('joined',({user})=>{
+          users[socket.id]=user;
+          console.log(`${user} has joined `);
+          socket.broadcast.emit('userJoined',{user:"Admin",message:` ${users[socket.id]} has joined`});
+          socket.emit('welcome',{user:"Admin",message:`Welcome to the chat,${users[socket.id]} `})
+    })
+   
+
+    socket.on('message',({message,id})=>{
+         io.emit('sendMessage',{user:users[id],message,id});
+    })
+
+    socket.on('disconnect',()=>{
+          socket.broadcast.emit('leave',{user:"Admin",message:`${users[socket.id]}  has left`});
+        console.log(`user left`);
+    })
+ });
+
+
